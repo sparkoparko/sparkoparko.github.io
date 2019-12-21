175a7fe2d1505845f119d925ee0325f3422ff4ba
YOUR_USERNAME <spark16@uw.edu>
2019-12-20 17:26:36 -0800

    register route

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.NetworkFirst()
+);
